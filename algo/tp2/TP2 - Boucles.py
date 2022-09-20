95f3f0383fe4d6027da26f121abf6785dbf949ba
IakoPain <95188886+IakoPain@users.noreply.github.com>
2022-09-20 09:53:38 +0200

    Add files via upload

---
+++ algo/tp2/TP2 - Boucles.py
@@ -0,0 +1,67 @@
+#------------------
+#Exo 1
+"""
+for n in range (1-1,15+1):
+    print (n)
+"""
+#------------------
+#Exo 2
+"""
+for n in range (-15,0+1):
+    print (n)
+"""
+#------------------
+#Exo 3
+"""
+n=int(input("Votre valeur de début : "))
+m=int(input("Votre valeur de fin : "))
+for g in range (n,m+1):
+    print(g)
+"""
+#------------------
+#Exo 4
+"""
+n=int(input("Votre valeur de début : "))
+for g in range (n,n+11):
+    print(g)
+"""
+#------------------
+#Exo 5
+"""
+p=0
+n=int(input("Quelle table de multiplication voulez vous ?"))
+j=n
+for i in range (1,12):
+    n=n*p
+    print(n)
+    p=p+1
+    n=j
+"""
+#------------------
+#Exo 6
+"""
+a=int(input("Valeur du Premier Nombre : "))
+b=int(input("Valeur du Deuxième Nombre : "))
+c=int(input("Valeur du Troismième Nombre : "))
+d=int(input("Valeur du Quatrième Nombre : "))
+e=int(input("Valeur du Cinquième Nombre : "))
+if a>b:
+    A=a
+else:
+    A=b
+if c>d:
+    B=c
+else:
+    B=d
+if A>B:
+    C=A
+else:
+    C=B
+if C>e:
+    D=C
+else:
+    D=e
+print("Le nombre le plus grand est :",D)
+"""
+#------------------
+#Exo 7
