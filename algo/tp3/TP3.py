4ae6d75d153733733be99e8779684f3033bc57f8
IakoPain <95188886+IakoPain@users.noreply.github.com>
2022-10-18 09:54:53 +0200

    Add files via upload

---
+++ algo/tp3/TP3.py
@@ -0,0 +1,29 @@
+#----------------------------------------
+#Exercice 1
+"""
+def afficheTexte():
+    print("Bonjour")
+afficheTexte()
+"""
+#----------------------------------------
+#Exercice 2
+"""
+def afficheTexte(prenom):
+    print("Bonjour",prenom)
+afficheTexte("Iako")
+"""
+#----------------------------------------
+#Exercice 3
+"""
+def somme(a,b):
+    s=a+b
+    return s
+print(somme(3,5))
+"""
+#----------------------------------------
+#Exercice 4
+
+def calculSurface(largeur,longueur):
+    surface=largeur*longueur
+    return surface
+print(calculSurface(10,5.2))
