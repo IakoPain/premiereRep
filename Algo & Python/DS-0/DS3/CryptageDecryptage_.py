c64442165a5a734157af1264a90554987219ada0
IakoPain <95188886+IakoPain@users.noreply.github.com>
2023-05-30 08:40:21 +0200

    Add files via upload

---
+++ Algo & Python/DS-0/DS3/CryptageDecryptage_.py
@@ -0,0 +1,23 @@
+﻿tableOrigine=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+tableCodee=  ['@','8','[','0','3','{','6','#','1','}',':','7','W','Z','*','?','O','%','$','-','V','^','M','+','/','N']
+
+
+def codage(phraseClair):
+    pass
+    #A compléter
+
+def decodage(phraseCodee):
+    pass
+    #A compléter
+
+
+menu=''
+while menu!='q':
+    print("------------------------------------------------------------")
+    print("1 : Codage d'une phrase")
+    print("2 : Décodage d'une phrase")
+    print("q : quitter")
+    print("------------------------------------------------------------")
+    menu=input("votre choix ?")
+    #A compléter
+
