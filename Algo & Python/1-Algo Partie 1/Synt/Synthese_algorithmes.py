ff646eb94fa7025c7be6eaa7b49293affca421b3
IakoPain <95188886+IakoPain@users.noreply.github.com>
2023-05-30 08:45:00 +0200

    Add files via upload

---
+++ Algo & Python/1-Algo Partie 1/Synt/Synthese_algorithmes.py
@@ -0,0 +1,37 @@
+#┍-------------------------------------------------------------┑
+#|                        Page 1/4                             |
+#└-------------------------------------------------------------┘
+"""
+a=4
+b=3.2
+c="hello"
+print(a,b,c," ->",end="")      #end = pas de retour à la ligne
+print (9)                      #affichage de variable avec retour a la ligne
+print ("bonjour")              #affichage de texte avec retour a la ligne
+"""
+#┍-------------------------------------------------------------┑
+#|                        Page 2/4                             |
+#└-------------------------------------------------------------┘
+"""
+a=1
+if a>10:                       #si a>10 alors :
+    print("ok")                #affiche "ok" et "test"
+    print("test")
+else:                          #sinon :
+    print("non")               #affiche "non" et la valeur de "a" +9
+    print(a+9)
+"""
+#┍-------------------------------------------------------------┑
+#|                        Page 3/4                             |
+#└-------------------------------------------------------------┘
+"""
+a=5                            #donne a "a" la valeur 5
+b=9                            #donne a "b" la valeur 9
+c=-3.6                         #donne a "c" la valeur -3.6
+
+a=a+5                          #donne a "a" sa valeur +5
+a=a*b                          #donne a "a" sa valeur * celle de "b"
+b=c**6                         #donne a "b" la value de c puissance 6
+c=c//b                         #donne a "c" sa valeur divisée entière par la valeur de "b"
+print(a,b,c)                   #affiche les valeurs de "a" "b" et "c"
+"""
