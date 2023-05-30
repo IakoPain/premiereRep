96fd0ea2841014a9b2b1373ff82ff063f5283093
IakoPain <95188886+IakoPain@users.noreply.github.com>
2023-05-30 08:55:54 +0200

    Add files via upload

---
+++ pyQT/Ex01-bouton/Ex11-dessin.py
@@ -0,0 +1,40 @@
+﻿#Ex11-dessin
+#tracer un point avec la souris
+#détecter une touche au clavier
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtWidgets, QtMultimedia, QtGui,uic
+import sys
+
+
+class MainWindows(QMainWindow):
+    def __init__(self):
+        super(MainWindows, self).__init__()
+        uic.loadUi('Ex11-dessin.ui', self)  #chargement du formulaire XML
+        self.setFixedSize(self.size())  #la fenêtre principale n'est pas modifiable
+        self.show()             #affiche la fenêtre MainWindows
+
+        self.position = QPoint(0,0)
+
+    def keyPressEvent(self, event): #une touche a été pressée
+        key = event.key()
+        if key == Qt.Key_M: #touche m ou M
+            print("touche 'm' pressée")
+        elif key == Qt.Key_Right:   #touche fleche droite
+            print("touche droite pressée")
+
+    def mousePressEvent(self, event):      #la souris est pressée
+        self.position = QPoint(event.x(),event.y()) #récupère la position de la souris
+        print(event.x(),event.y())  #affiche les coordonnées
+        self.update()   #déclenche paintEvent
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.blue, 5)) #definir un crayon
+        painter.drawPoint(self.position)    #affciher un point aux coordonnées position
+
+app = QApplication(sys.argv)
+window = MainWindows()
+app.exec_()
---
+++ pyQT/Ex01-bouton/Ex12-graphics.py
@@ -0,0 +1,42 @@
+﻿#Ex12-graphics
+#tracé de formes géométriques
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtWidgets, QtMultimedia, QtGui,uic
+import sys
+
+class MainWindows(QMainWindow):
+    def __init__(self):
+        super(MainWindows, self).__init__()
+        uic.loadUi('Ex12-graphics.ui', self)  #chargement du formulaire XML
+        self.setFixedSize(self.size())  #la fenêtre principale n'est pas modifiable
+        self.scene = QGraphicsScene(self) #instancie un scene
+        self.brush= QBrush(Qt.lightGray)  #couleur de remplissage
+        self.pen = QPen()                  #crayon
+        #implante la scene dans la vue graphique
+        self.scene.setSceneRect(0, 0, self.graphicsView.width(), self.graphicsView.height())
+        #couleur de fond
+        self.scene.setBackgroundBrush(self.brush)
+        self.graphicsView.setScene(self.scene)
+        #Quand on clique sur le pushButton, on appelle la méthode boutonOK
+        self.pushButton.clicked.connect(self.boutonOk)
+        self.show()             #affiche la fenêtre MainWindows
+
+    def boutonOk(self):
+        print('bouton cliqué')
+        self.pen.setWidth(2)
+        self.brush.setColor(Qt.red)
+        self.scene.addLine(0,0,50,200,self.pen)
+        self.scene.addRect(30,30, 50,50,self.pen,self.brush)
+        #lst=self.scene.items()
+        #self.scene.removeItem(lst[1])
+        self.font=QFont("Fixed",20)
+        self.texte = self.scene.addText("uwu",self.font)
+        self.texte.setDefaultTextColor(Qt.blue)
+        self.texte.setPos(50,100)
+
+app = QApplication(sys.argv)
+window = MainWindows()
+app.exec_()
