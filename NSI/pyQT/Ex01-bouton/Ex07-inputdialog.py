3f6025668f49d38e1865c074759468a79cd22b6b
IakoPain <95188886+IakoPain@users.noreply.github.com>
2024-06-06 09:15:45 +0200

    Add files via upload

---
+++ NSI/pyQT/Ex01-bouton/Ex07-inputdialog.py
@@ -0,0 +1,28 @@
+﻿from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtWidgets, QtMultimedia, QtGui,uic
+import sys
+
+
+class MainWindows(QMainWindow):
+    def __init__(self):
+        super(MainWindows, self).__init__()
+        uic.loadUi('Ex01-bouton.ui', self)  #chargement du formulaire XML
+        self.setFixedSize(self.size())  #la fenêtre principale n'est pas modifiable
+        #Quand on clique sur le pushButton, on appelle la méthode boutonOK
+        self.pushButton.clicked.connect(self.boutonOk)
+        self.show()             #affiche la fenêtre MainWindows
+
+    def boutonOk(self):
+        num, ok = QInputDialog.getInt(self, "Entier", "Saisir un nombre entier")
+        print("nombre entier saisi ", num, ok)
+        dec, ok = QInputDialog.getDouble(self, "décimal", "Saisir un nombre décimal")
+        print("nombre décimal saisi ", dec, ok)
+        text, ok = QInputDialog.getText(self, 'Text', 'Enter your name:')
+        print("texte saisi ", text, ok)
+
+
+app = QApplication(sys.argv)
+window = MainWindows()
+app.exec_()
