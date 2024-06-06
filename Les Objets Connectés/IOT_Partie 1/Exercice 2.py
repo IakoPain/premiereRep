186e83623d05a179e5f9af0dcde34f732ed8d354
IakoPain <95188886+IakoPain@users.noreply.github.com>
2024-06-06 09:06:44 +0200

    Delete Les Objets Connectés directory
    moving to NSI

--- Les Objets Connectés/IOT_Partie 1/Exercice 2.py
+++
@@ -1,9 +0,0 @@
-from machine import ADC
-from time import sleep
-
-adc = ADC(0)
-
-while True:
-    valeur=adc.read()
-    print(valeur)
-    sleep(1)
--- Les Objets Connectés/IOT_Partie 1/Exercice 8.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin,I2C
-from ssd1306 import SSD1306_I2C
-i2c = I2C(scl=Pin(5), sda=Pin(4))
-
-oled = SSD1306_I2C(128, 64, i2c, 0x3c)
-oled.fill(0)
-oled.text("Hello NSI", 28, 32)
-oled.show()
